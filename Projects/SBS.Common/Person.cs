7a72013358414e9a4525f5b62bc63370190802f6
Brent Acs <brent.mann@gmail.com>
2021-03-18 10:21:44 -0400

    Projects setup, csv reading complete,

---
+++ Projects/SBS.Common/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SBS.Common
+{
+  public class Person
+  {
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+  }
+}
--- Projects/Sender/Program.cs
+++ Projects/Sender/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using SBS.Common;
 
 namespace Sender
 {
@@ -7,6 +11,19 @@ namespace Sender
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World, I'm Sender!");
+
+      // TODO: add support for accepting command line file(s)
+      string csvPath = @"./people.csv";
+      if (!File.Exists(csvPath))
+      {
+        Console.WriteLine($"Input file doesn't exist.");
+      }
+
+      using var reader = new StreamReader(csvPath);
+      using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+      var records = csv.GetRecords<Person>();
+
+      Console.ReadKey();
     }
   }
 }
